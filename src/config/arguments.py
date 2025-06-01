3e11e772504802e040452fa77cb13c1f9f712621
solidshen <solidshen519@gmail.com>
2025-06-01 02:35:00 -0400

    update hyperparameters

--- src/config/arguments.py
+++ src/config/arguments.py
@@ -33,17 +33,13 @@ class BAITArguments:
     do_sample: bool = field(default=False, metadata={"help": "Whether to use sampling in generation"})
     return_dict_in_generate: bool = field(default=True, metadata={"help": "Whether to return a dict in generation"})
     output_scores: bool = field(default=True, metadata={"help": "Whether to output scores"})
-    min_target_len: int = field(default=5, metadata={"help": "Minimum length of target sequence"})
+    min_target_len: int = field(default=4, metadata={"help": "Minimum length of target sequence"})
     self_entropy_lower_bound: float = field(default=1, metadata={"help": "Lower bound of self entropy"})
     self_entropy_upper_bound: float = field(default=2.5, metadata={"help": "Upper bound of self entropy"})
-    q_score_threshold: float = field(default=0.9, metadata={"help": "Q-score threshold"})
+    q_score_threshold: float = field(default=0.8, metadata={"help": "Q-score threshold"})
     judge_model_name: str = field(default="gpt-4o", metadata={"help": "Judge model name, currently only support OpenAI models"})
     max_retries: int = field(default=3, metadata={"help": "Maximum number of retry attempts"})
     retry_delay: float = field(default=1.0, metadata={"help": "Delay between retries in seconds"})
-    # output_dir: str = field(default="", metadata={"help": "Output directory"})
-    # project_name: str = field(default="", metadata={"help": "Project name"})
-    # report_to: str = field(default="", metadata={"help": "Report to", "choices": ["wandb", ""]})
-
 
 
 @dataclass
